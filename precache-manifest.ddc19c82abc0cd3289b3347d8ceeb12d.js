ddc53e7b87526aead6c90149b9ba67e0d71ce918
Brett Williams <brett@brettwilliams.net>
2020-10-08 13:21:38 -0500

    Updates

---
+++ precache-manifest.ddc19c82abc0cd3289b3347d8ceeb12d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe9bbea20cd5008ce3f11702b770392",
+    "url": "/ReactDragDropDesign/index.html"
+  },
+  {
+    "revision": "d727143da3abdd259293",
+    "url": "/ReactDragDropDesign/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b737314f3af13609731c",
+    "url": "/ReactDragDropDesign/static/js/2.9d90d81a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDragDropDesign/static/js/2.9d90d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d727143da3abdd259293",
+    "url": "/ReactDragDropDesign/static/js/main.5bf79174.chunk.js"
+  },
+  {
+    "revision": "1976a3eee9199a45de65",
+    "url": "/ReactDragDropDesign/static/js/runtime-main.fd7b92e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactDragDropDesign/static/media/logo.5d5d9eef.svg"
+  }
+]);
