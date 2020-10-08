92f26423daa9412f6a4398fdf70e4a64a3300181
Brett Williams <brett@brettwilliams.net>
2020-10-08 15:51:53 -0500

    added react-moveable

--- src/components/DesignCanvas.jsx
+++ src/components/DesignCanvas.jsx
@@ -1,68 +1,41 @@
 import React, { useState, useContext, useEffect } from "react";
 import Moveable from "react-moveable";
-import {DroppedElement} from "./DroppedContext"
+import DesignEditable from "./DesignEditable"
+import { DroppedElement } from "./DroppedContext"
 import { imgPaths } from "../data/DesignImages"
-/*
-import { motion } from "framer-motion";
-import DesignSection from "./components/DesignSection"
-import {imgPaths} from "./data/DesignImages"
-*/
+
 
 export default function DesignCanvas(){
 
+    const [dropArr   , setDropArr   ] = useState([])
+    const [selected  , setSelected  ] = useState();
+    const [tempFrame , setTempFrame ] = useState();
+
     const {dropped} = useContext(DroppedElement);
-    const [dropArr, setDropArr] = useState([])
+    const DesignEditableArr = dropArr.map( DE => <DesignEditable key={DE.id} id={DE.id} path={DE.path} frame={DE.frame} set={setSelected}/> )
 
     useEffect(() => {
         dropped && setDropArr( dA => [...dA, dropped])
     }, [dropped])
 
-    const DroppedDesign = ({id,path,frame, set}) => {
-        const selectMe = (e) => {/* set(e.target) */}
-        return(
-          <div
-          className="draggable"
-          onClick={selectMe}
-          style={{transform:`translate(${frame[0]}px, ${frame[1]}px) scale(${frame[2]}, ${frame[2]}) rotate(${frame[3]}deg)`}}
-          data-frame="0, 0, 1, 0" >
-          <img style={{pointerEvents:'none'}} src={path} alt={id} />
-        </div>
-        ) 
-    };
-
-    /*
-    const [selected  , setSelected  ] = useState();
-    const [tempFrame , setTempFrame ] = useState({
-      translate :[ 0 , 0 ] ,
-      scale     :[ 1 , 1 ] ,
-      rotate    :0
-    });
-
-    const DroppedDesign = ({img,frame, set}) => {
-      const selectMe = (e) => { set(e.target) }
-      return(
-        <div
-        className="draggable"
-        onClick={selectMe}
-        style={{transform:`translate(${frame[0]}px, ${frame[1]}px) scale(${frame[2]}, ${frame[2]}) rotate(${frame[3]}deg)`}}
-        data-frame="0, 0, 1, 0" >
-        <img src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/40041/${img}`} alt={img} />
-      </div>
-      ) 
-    };
     const thisStartEvent = (e, type) => {
+
       const F = e.target.dataset.frame.split(",").map( n => parseFloat(n) )
       const updatedFrame = {
         translate :[ F[0] , F[1] ] ,
         scale     :[ F[2] , F[2] ] ,
         rotate    :F[3]
       }
+      setTempFrame(updatedFrame);
+
+      console.log(updatedFrame, type)
       type === "drag"   && e.set(updatedFrame.translate);
       type === "rotate" && e.set(updatedFrame.rotate);
       type === "scale"  && e.set(updatedFrame.scale);
   
       e.dragStart && e.dragStart.set(updatedFrame.translate);
-      setTempFrame(updatedFrame);
+      e.target.parentNode.append(e.target)
+
       console.log(tempFrame)
     };
     const thisUpdateEvent = (e) => {
@@ -79,12 +52,10 @@ export default function DesignCanvas(){
     };  
     const thisEndEvent = (e)=>{
       e.target.setAttribute('data-frame', `${tempFrame.translate[0]},${tempFrame.translate[1]},${tempFrame.scale[0]},${tempFrame.rotate}`)
-    }
-    //{get().map( (e,i) => <DroppedDesign key={`dropped_${i}`} img={e.img} frame={e.frame} set={setSelected} />)}
-    console.log('test',{dropped})
-
-    const MoveableElement = () =>(
-        <Moveable
+    };
+    return (
+      <div className="designCanvas">
+          <Moveable
             target={selected}
             renderDirections={["nw","ne","w","e","sw","s","se"]}
             edge={false}
@@ -117,11 +88,7 @@ export default function DesignCanvas(){
             //PINCHING
             pinchable={true}
         />
-    )
-    */
-    return (
-      <div className="designCanvas">
-          {dropArr.map( (DE) => <DroppedDesign key={DE.id} id={DE.id} path={DE.path} frame={DE.frame} set={'g'}/> )}
+        {DesignEditableArr}
       </div>
     ) ;
   }
--- src/components/DesignEditable.jsx
+++ src/components/DesignEditable.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-export default function DesignEditable({img,frame, set}) {
-    const selectMe = (e) => { set(e.target) }
-    return (
-        <div
-        className="draggable"
-        onClick={selectMe}
-        style={{transform:`translate(${frame[0]}px, ${frame[1]}px) scale(${frame[2]}, ${frame[2]}) rotate(${frame[3]}deg)`}}
-        data-frame="0, 0, 1, 0" >
-        <img src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/40041/${img}`} alt={img} />
-      </div>
-    );
-}
+export default function DesignEditable ({id,path,frame, set}){
+  const selectMe = (e) => { 
+    set(e.target) 
+  }
+  return(
+    <div
+      className="draggable"
+      onClick={selectMe}
+      style={{transform:`translate(${frame[0]}px, ${frame[1]}px) scale(${frame[2]}, ${frame[2]}) rotate(${frame[3]}deg)`}}
+      data-frame={frame} >
+    <img style={{pointerEvent:"none"}}  src={path} alt={id} />
+  </div>
+  ) 
+};
